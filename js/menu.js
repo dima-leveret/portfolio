db2526d8d6115b8d5b72255888d8bb9ce339d173
Dima <dimazayac@gmail.com>
2021-04-22 19:34:00 +0200

    my actual portfolio

---
+++ js/menu.js
@@ -0,0 +1,33 @@
+const burger = document.querySelector('#burger');
+
+const mobileUlContainer = document.querySelector('.mobileUlContainer');
+
+const desktopUl = document.querySelector('.desktopUl');
+const desktopUla = document.querySelectorAll('.desktopUl a');
+
+const mobileUl = document.querySelector('.mobileUl');
+const mobileUla = document.querySelectorAll('.mobileUl a')
+
+
+
+// desktopUla.forEach(a => {
+//     a.addEventListener('click', () => {
+//         desktopUl.querySelector('.activeTab').classList.remove('activeTab');
+//         a.classList.add('activeTab')
+//     })
+// })
+
+mobileUla.forEach(a => {
+    a.addEventListener('click', () => {
+        // mobileUl.querySelector('.activeTab').classList.remove('activeTab');
+        // a.classList.add('activeTab');
+        burger.classList.toggle('activeBurger');
+        mobileUlContainer.classList.toggle('activeUl')
+    })
+})
+
+
+burger.onclick = function() {
+    burger.classList.toggle('activeBurger');
+    mobileUlContainer.classList.toggle('activeUl')
+}
